746142d7d8b833dc7427aa7140fbae9b1cb97146
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-08-22 13:21:41 -0500

    done with rps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+  const choices = ["rock", "paper", "scissors"];
+
+  const handleClick = (value) => {
+    setUserChoice(value);
+    generateComputerChoice();
+  };
+
+  const generateComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    const randomChoice = choices[randomNumber];
+    setComputerChoice(randomChoice);
+    checkResult();
+  };
+
+  useEffect(() => {
+    {
+      switch (userChoice + computerChoice) {
+        case "scissorspaper":
+        case "rockscissors":
+        case "paperrock":
+          setResult("YOU WIN!");
+          break;
+        case "paperscissors":
+        case "scissorsrock":
+        case "rockpaper":
+          setResult("YOU LOSE!");
+          break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorscissors":
+          setResult("IT'S A DRAW!");
+          break;
+      }
+    }
+  }, [computerChoice, userChoice]);
+
+  const checkResult = () => {
+    switch (userChoice + computerChoice) {
+      case "scissorspaper":
+      case "rockscissors":
+      case "paperrock":
+        setResult("YOU WIN!");
+        break;
+      case "paperscissors":
+      case "scissorsrock":
+      case "rockpaper":
+        setResult("YOU LOSE!");
+        break;
+      case "rockrock":
+      case "paperpaper":
+      case "scissorscissors":
+        setResult("IT'S A DRAW!");
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> user choice is: {userChoice}</h1>
+      <h1>computer choice is: {computerChoice} </h1>
+      {choices.map((choice, index) => (
+        <button key={index} onClick={() => handleClick(choice)}>
+          {choice}
+        </button>
+      ))}
+      <h1>{result}</h1>
     </div>
   );
 }
